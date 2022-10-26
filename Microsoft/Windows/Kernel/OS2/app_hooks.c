018aa034ae0152636e80248fa37e9b554ac8ab7e
hao0112 <haowoo0112@gmail.com>
2022-10-26 17:17:10 +0800

    initial commit

---
+++ Microsoft/Windows/Kernel/OS2/app_hooks.c
@@ -0,0 +1,328 @@
+/*
+*********************************************************************************************************
+*                                            EXAMPLE CODE
+*
+*               This file is provided as an example on how to use Micrium products.
+*
+*               Please feel free to use any application code labeled as 'EXAMPLE CODE' in
+*               your application products.  Example code may be used as is, in whole or in
+*               part, or may be used as a reference only. This file can be modified as
+*               required to meet the end-product requirements.
+*
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*
+*               You can find our product's user manual, API reference, release notes and
+*               more information at https://doc.micrium.com.
+*               You can contact us at www.micrium.com.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                              uC/OS-II
+*                                          Application Hooks
+*
+* Filename : app_hooks.c
+* Version  : V2.92.13
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                            INCLUDE FILES
+*********************************************************************************************************
+*/
+
+#include  <os.h>
+
+
+/*
+*********************************************************************************************************
+*                                      EXTERN  GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                           LOCAL CONSTANTS
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                          LOCAL DATA TYPES
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                            LOCAL TABLES
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                       LOCAL GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                      LOCAL FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+
+
+/*
+*********************************************************************************************************
+*********************************************************************************************************
+**                                         GLOBAL FUNCTIONS
+*********************************************************************************************************
+*********************************************************************************************************
+*/
+
+void OutFileInit() {
+    /*Clear the file*/
+    if ((Output_err = fopen_s(&Output_fp, OUTPUT_FULE_NAME, "w")) == 0)
+        fclose(Output_fp);
+    else
+        printf("Error to clear output file");
+}
+
+void InputFile() {
+    /*
+    * Read File
+    * Task Information;
+    * Task_ID ArriveTime ExecutionTime Periodic
+    */
+    errno_t err;
+    if ((err = fopen_s(&fp, INPUT_FULE_NAME, "r")) == 0)    /*task set 1-4*/
+    {
+        printf("The file 'TaskSet.txt' was opened\n");
+    }
+    else
+    {
+        printf("The file 'TaskSet.txt' was not opened\n");
+    }
+
+    char str[MAX];
+    char* ptr;
+    char* pTmp = NULL;
+    int TaskInfo[INFO], i, j = 0;
+    TASK_NUMBER = 0;
+    while (!feof(fp))
+    {
+        i = 0;
+        memset(str, 0, sizeof(str));
+        fgets(str, sizeof(str) - 1, fp);
+        ptr = strtok_s(str, " ", &pTmp);
+        while (ptr != NULL)
+        {
+            TaskInfo[i] = atoi(ptr);
+            ptr = strtok_s(NULL, " ", &pTmp);
+            /*printf("Info: %d\n", task_inf[i]);*/
+            if (i == 0) {
+                TASK_NUMBER++;
+                TaskParameter[j].TaskID = TASK_NUMBER;
+            }
+            else if (i == 1)
+                TaskParameter[j].TaskArriveTime = TaskInfo[i];
+            else if (i == 2) {
+                TaskParameter[j].TaskExecutionTime = TaskInfo[i];
+            }
+            else if (i == 3)
+                TaskParameter[j].TaskPeriodic = TaskInfo[i];
+
+            i++;
+        }
+
+        /*Initial Priority*/
+        TaskParameter[j].TaskPriority = j; //just an example
+
+        j++;
+    }
+    fclose(fp);
+    /*read file*/
+}
+/*
+*********************************************************************************************************
+*********************************************************************************************************
+**                                        uC/OS-II APP HOOKS
+*********************************************************************************************************
+*********************************************************************************************************
+*/
+
+#if (OS_APP_HOOKS_EN > 0)
+
+/*
+*********************************************************************************************************
+*                                  TASK CREATION HOOK (APPLICATION)
+*
+* Description : This function is called when a task is created.
+*
+* Argument(s) : ptcb   is a pointer to the task control block of the task being created.
+*
+* Note(s)     : (1) Interrupts are disabled during this call.
+*********************************************************************************************************
+*/
+
+void  App_TaskCreateHook (OS_TCB *ptcb)
+{
+#if (APP_CFG_PROBE_OS_PLUGIN_EN == DEF_ENABLED) && (OS_PROBE_HOOKS_EN > 0)
+    OSProbe_TaskCreateHook(ptcb);
+#endif
+}
+
+
+/*
+*********************************************************************************************************
+*                                  TASK DELETION HOOK (APPLICATION)
+*
+* Description : This function is called when a task is deleted.
+*
+* Argument(s) : ptcb   is a pointer to the task control block of the task being deleted.
+*
+* Note(s)     : (1) Interrupts are disabled during this call.
+*********************************************************************************************************
+*/
+
+void  App_TaskDelHook (OS_TCB *ptcb)
+{
+    (void)ptcb;
+}
+
+
+/*
+*********************************************************************************************************
+*                                    IDLE TASK HOOK (APPLICATION)
+*
+* Description : This function is called by OSTaskIdleHook(), which is called by the idle task.  This hook
+*               has been added to allow you to do such things as STOP the CPU to conserve power.
+*
+* Argument(s) : none.
+*
+* Note(s)     : (1) Interrupts are enabled during this call.
+*********************************************************************************************************
+*/
+
+#if OS_VERSION >= 251
+void  App_TaskIdleHook (void)
+{
+}
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                  STATISTIC TASK HOOK (APPLICATION)
+*
+* Description : This function is called by OSTaskStatHook(), which is called every second by uC/OS-II's
+*               statistics task.  This allows your application to add functionality to the statistics task.
+*
+* Argument(s) : none.
+*********************************************************************************************************
+*/
+
+void  App_TaskStatHook (void)
+{
+}
+
+
+/*
+*********************************************************************************************************
+*                                   TASK RETURN HOOK (APPLICATION)
+*
+* Description: This function is called if a task accidentally returns.  In other words, a task should
+*              either be an infinite loop or delete itself when done.
+*
+* Arguments  : ptcb      is a pointer to the task control block of the task that is returning.
+*
+* Note(s)    : none
+*********************************************************************************************************
+*/
+
+
+#if OS_VERSION >= 289
+void  App_TaskReturnHook (OS_TCB  *ptcb)
+{
+    (void)ptcb;
+}
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                   TASK SWITCH HOOK (APPLICATION)
+*
+* Description : This function is called when a task switch is performed.  This allows you to perform other
+*               operations during a context switch.
+*
+* Argument(s) : none.
+*
+* Note(s)     : (1) Interrupts are disabled during this call.
+*
+*               (2) It is assumed that the global pointer 'OSTCBHighRdy' points to the TCB of the task that
+*                   will be 'switched in' (i.e. the highest priority task) and, 'OSTCBCur' points to the
+*                  task being switched out (i.e. the preempted task).
+*********************************************************************************************************
+*/
+
+#if OS_TASK_SW_HOOK_EN > 0
+void  App_TaskSwHook (void)
+{
+#if (APP_CFG_PROBE_OS_PLUGIN_EN > 0) && (OS_PROBE_HOOKS_EN > 0)
+    OSProbe_TaskSwHook();
+#endif
+}
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                   OS_TCBInit() HOOK (APPLICATION)
+*
+* Description : This function is called by OSTCBInitHook(), which is called by OS_TCBInit() after setting
+*               up most of the TCB.
+*
+* Argument(s) : ptcb    is a pointer to the TCB of the task being created.
+*
+* Note(s)     : (1) Interrupts may or may not be ENABLED during this call.
+*********************************************************************************************************
+*/
+
+#if OS_VERSION >= 204
+void  App_TCBInitHook (OS_TCB *ptcb)
+{
+    (void)ptcb;
+}
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                       TICK HOOK (APPLICATION)
+*
+* Description : This function is called every tick.
+*
+* Argument(s) : none.
+*
+* Note(s)     : (1) Interrupts may or may not be ENABLED during this call.
+*********************************************************************************************************
+*/
+
+#if OS_TIME_TICK_HOOK_EN > 0
+void  App_TimeTickHook (void)
+{
+#if (APP_CFG_PROBE_OS_PLUGIN_EN == DEF_ENABLED) && (OS_PROBE_HOOKS_EN > 0)
+    OSProbe_TickHook();
+#endif
+}
+#endif
+#endif
---
+++ Microsoft/Windows/Kernel/OS2/main.c
@@ -0,0 +1,212 @@
+/*
+*********************************************************************************************************
+*                                            EXAMPLE CODE
+*
+*               This file is provided as an example on how to use Micrium products.
+*
+*               Please feel free to use any application code labeled as 'EXAMPLE CODE' in
+*               your application products.  Example code may be used as is, in whole or in
+*               part, or may be used as a reference only. This file can be modified as
+*               required to meet the end-product requirements.
+*
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*
+*               You can find our product's user manual, API reference, release notes and
+*               more information at https://doc.micrium.com.
+*               You can contact us at www.micrium.com.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                              uC/OS-II
+*                                            EXAMPLE CODE
+*
+* Filename : main.c
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                            INCLUDE FILES
+*********************************************************************************************************
+*/
+
+#include  <cpu.h>
+#include  <lib_mem.h>
+#include  <os.h>
+
+#include  "app_cfg.h"
+
+
+/*
+*********************************************************************************************************
+*                                            LOCAL DEFINES
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                       LOCAL GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+static  OS_STK  StartupTaskStk[APP_CFG_STARTUP_TASK_STK_SIZE];
+#define TASK_STACKSIZE 2048
+
+/*
+*********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+static  void  task1(void* p_arg);
+static  void  task2(void* p_arg);
+static  void  StartupTask (void  *p_arg);
+
+
+/*
+*********************************************************************************************************
+*                                                main()
+*
+* Description : This is the standard entry point for C code.  It is assumed that your code will call
+*               main() once you have performed all necessary initialization.
+*
+* Arguments   : none
+*
+* Returns     : none
+*
+* Notes       : none
+*********************************************************************************************************
+*/
+
+int  main (void)
+{
+#if OS_TASK_NAME_EN > 0u
+    CPU_INT08U  os_err;
+#endif
+
+
+    CPU_IntInit();
+
+    Mem_Init();                                                 /* Initialize Memory Managment Module                   */
+    CPU_IntDis();                                               /* Disable all Interrupts                               */
+    CPU_Init();                                                 /* Initialize the uC/CPU services                       */
+
+    
+
+    /*Initialize Output File*/
+    OutFileInit();
+
+    /*Input File*/
+    InputFile();
+
+    OSInit();                                                   /* Initialize uC/OS-II                                  */
+
+    /*Dynamic Create the Stack size*/
+    Task_STK = malloc(TASK_NUMBER * sizeof(int*));
+
+    /* for each pointer, allocate storage for an array of ints*/
+    int n;
+    OS_TCB* ptcb;
+    for (n = 0; n < TASK_NUMBER; n++){
+        Task_STK[n] = malloc(TASK_STACKSIZE * sizeof(int));
+    }
+    /*Create Task Set*/
+    OSTaskCreateExt(task1,                               /* Create the task1*/
+        &TaskParameter[0],
+        &Task_STK[0][TASK_STACKSIZE - 1],
+        TaskParameter[0].TaskPriority,
+        TaskParameter[0].TaskID,
+        &Task_STK[0][0],
+        TASK_STACKSIZE,
+        &TaskParameter[0],
+        (OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+    OSTaskCreateExt(task2,                               /* Create the task1*/
+        &TaskParameter[1],
+        &Task_STK[1][TASK_STACKSIZE - 1],
+        TaskParameter[1].TaskPriority,
+        TaskParameter[1].TaskID,
+        &Task_STK[1][0],
+        TASK_STACKSIZE,
+        &TaskParameter[1],
+        (OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+    
+    
+    OSTimeSet(0);
+    OSStart();                                                  /* Start multitasking (i.e. give control to uC/OS-II)   */
+
+    while (DEF_ON) {                                            /* Should Never Get Here.                               */
+        ;
+    }
+}
+
+void  task1(void* p_arg) {
+    task_para_set* task_data;
+    task_data = p_arg;
+    while (1) 
+    {/*
+        printf("Tick: %d, Hello from task%d\n", OSTime, task_data->TaskID);
+        if ((Output_err = fopen_s(&Output_fp, "./Output.txt", "a")) == 0)
+        {
+            fprintf(Output_fp, "Tick: %d, Hello from task%d\n", OSTime, task_data->TaskID);
+            fclose(Output_fp);
+        }*/
+        OSTimeDly(task_data->TaskPeriodic);
+    }
+}
+
+void  task2(void* p_arg) {
+    task_para_set* task_data;
+    task_data = p_arg;
+    while (1)
+    {/*
+        printf("Tick: %d, Hello from task%d\n", OSTime, task_data->TaskID);
+        if ((Output_err = fopen_s(&Output_fp, "./Output.txt", "a")) == 0)
+        {
+            fprintf(Output_fp, "Tick: %d, Hello from task%d\n", OSTime, task_data->TaskID);
+            fclose(Output_fp);
+        }*/
+        OSTimeDly(task_data->TaskPeriodic);
+    }
+}
+/*
+*********************************************************************************************************
+*                                            STARTUP TASK
+*
+* Description : This is an example of a startup task.  As mentioned in the book's text, you MUST
+*               initialize the ticker only once multitasking has started.
+*
+* Arguments   : p_arg   is the argument passed to 'StartupTask()' by 'OSTaskCreate()'.
+*
+* Returns     : none
+*
+* Notes       : 1) The first line of code is used to prevent a compiler warning because 'p_arg' is not
+*                  used.  The compiler should not generate any code for this statement.
+*********************************************************************************************************
+*/
+
+static  void  StartupTask (void *p_arg)
+{
+   (void)p_arg;
+
+    OS_TRACE_INIT();                                            /* Initialize the uC/OS-II Trace recorder               */
+
+#if OS_CFG_STAT_TASK_EN > 0u
+    OSStatTaskCPUUsageInit(&err);                               /* Compute CPU capacity with no task running            */
+#endif
+
+#ifdef CPU_CFG_INT_DIS_MEAS_EN
+    CPU_IntDisMeasMaxCurReset();
+#endif
+    
+    APP_TRACE_DBG(("uCOS-III is Running...\n\r"));
+
+    while (DEF_TRUE) {                                          /* Task body, always written as an infinite loop.       */
+        OSTimeDlyHMSM(0u, 0u, 1u, 0u);
+		APP_TRACE_DBG(("Time: %d\n\r", OSTimeGet()));
+    }
+}
+
